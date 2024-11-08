3728176d97bad9a20b75224a0cc597cb8a7251aa
Maximilian Schmailzl <maximilian@schmailzl.net>
2024-11-08 15:37:46 +0100

    AL-55 functions to merge channels and getting the pixel value

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -36,3 +36,16 @@ class ImageUtils:
         img = cv2.imread(source)
         cropped = img[y - radius : y + radius + 1, x - radius : x + radius + 1]
         cv2.imwrite(target, cropped)
+
+    @staticmethod
+    def merge_channels(image_blue: str, image_green: str, image_red: str, target: str):
+        blue = cv2.imread(image_blue, 0)
+        green = cv2.imread(image_green, 0)
+        red = cv2.imread(image_red, 0)
+        merged = cv2.merge((blue, green, red)) * 3
+        cv2.imwrite(target, merged)
+
+    @staticmethod
+    def get_pixel_value(image: str, x: int, y: int):
+        img = cv2.imread(image, 0)
+        return img[y, x]
