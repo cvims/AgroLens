dc656e6e89819c3401f4cb11e81bdefb7ed831ec
Maximilian Schmailzl <maximilian@schmailzl.net>
2024-11-19 13:19:59 +0100

    AL-73 satellite data structure (#6)
    Co-authored-by: Marco - resolved 6 conflicts

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -3,8 +3,25 @@ from osgeo import gdal, osr
 
 
 class ImageUtils:
+    """
+    Various methods for loading and altering image data
+    """
+
     @staticmethod
-    def location_to_pixel(image: str, latitude: float, longitude: float):
+    def location_to_pixel(
+        image: str, latitude: float, longitude: float
+    ) -> tuple[int, int]:
+        """
+        Calculates the pixel coordinates inside the given image from GPS coordinates
+
+        Parameters:
+            image (str): Path of the image
+            latitude (float): GPS latitude
+            longitude (float): GPS longitude
+
+        Returns:
+            tuple[int, int]: The pixel x and y coordinates in the image
+        """
         gdal.UseExceptions()
         dataset = gdal.Open(image)
         if not dataset:
@@ -26,19 +43,71 @@ class ImageUtils:
         return pixel_x, pixel_y
 
     @staticmethod
-    def crop(source: str, target: str, x: int, y: int, width: int, height: int):
+    def crop(source: str, target: str, x: int, y: int, width: int, height: int) -> None:
+        """
+        Crops a image to the given size
+
+        Parameters:
+            source (str): Path of the source image
+            target (str): Path to save the cropped image
+            x (int): Start of the cut (X)
+            y (int): Start of the cut (Y)
+            width (int): Cropped width
+            height (int): Cropped height
+        """
         img = cv2.imread(source)
         cropped = img[y : y + height, x : x + width]
         cv2.imwrite(target, cropped)
 
     @staticmethod
-    def crop_center(source: str, target: str, x: int, y: int, radius: int):
+    def crop_center(source: str, target: str, x: int, y: int, radius: int) -> None:
+        """
+        Crops the image to a given radius around a center point (in pixels).
+        The given point is in the exact center of the cropped image.
+
+        Parameters:
+            source (str): Path of the source image
+            target (str): Path to save the cropped image
+            x (int): Center point (X)
+            y (int): Center point (Y)
+            radius (int): Radius (determines the image size -> 2 x radius + 1)
+        """
         img = cv2.imread(source)
         cropped = img[y - radius : y + radius + 1, x - radius : x + radius + 1]
         cv2.imwrite(target, cropped)
 
+    @classmethod
+    def crop_location(
+        cls, source: str, target: str, latitude: float, longitude: float, radius: int
+    ) -> None:
+        """
+        Crops the image to a given radius around a center point (GPS location).
+        The given point is in the exact center of the cropped image.
+
+        Parameters:
+            source (str): Path of the source image
+            target (str): Path to save the cropped image
+            latitude (float): GPS latitude of the center point
+            longitude (float): GPS longitude of the center point
+            radius (int): Radius in pixels (determines the image size -> 2 x radius + 1)
+        """
+        x, y = cls.location_to_pixel(source, latitude, longitude)
+        cls.crop_center(source, target, x, y, radius)
+
     @staticmethod
-    def merge_channels(image_blue: str, image_green: str, image_red: str, target: str):
+    def merge_channels(
+        image_blue: str, image_green: str, image_red: str, target: str
+    ) -> None:
+        """
+        Generates and saves a true color (RGB) image from three
+        separate grayscale images representing red, green and blue channels.
+
+        Parameters:
+            image_blue (str): Path of the image representing the blue channel
+            image_green (str): Path of the image representing the green channel
+            image_red (str): Path of the image representing the red channel
+            target (str): Path to save the resulting RGB image
+        """
         blue = cv2.imread(image_blue, 0)
         green = cv2.imread(image_green, 0)
         red = cv2.imread(image_red, 0)
@@ -46,6 +115,17 @@ class ImageUtils:
         cv2.imwrite(target, merged)
 
     @staticmethod
-    def get_pixel_value(image: str, x: int, y: int):
+    def get_pixel_value(image: str, x: int, y: int) -> int:
+        """
+        Returns the value of the given pixel of a grayscale image.
+
+        Parameters:
+            image (str): Path of the image
+            x (int): Pixel x-coordinate
+            y (int): Pixel y-coordinate
+
+        Returns:
+            int: Grayscale integer value of the pixel
+        """
         img = cv2.imread(image, 0)
         return img[y, x]
--- utils/sentinel_api.py
+++ utils/sentinel_api.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import glob
 import os
 from pathlib import Path
 import re
@@ -7,33 +8,54 @@ import shutil
 import sys
 import time
 from zipfile import ZipFile
+from utils.image_utils import ImageUtils
 import rasterio
 import numpy as np
 
 
+
 class SentinelApi:
+    """
+    Methods to query and download Sentinel 2 data
+    """
+
     AUTH_URL = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
     API_URL = "https://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json"
     DOWNLOAD_URL = (
         "https://zipper.dataspace.copernicus.eu/odata/v1/Products(#product_id#)/$value"
     )
 
-    @staticmethod
+    @classmethod
     def get_data(
+        cls,
         date: str,
         latitude: float,
         longitude: float,
         span: int = 30,
         cloudCover: int = 50,
         limit: int = 1,
-    ):
-        __class__.authenticate()
+    ) -> list[dict]:
+        """
+        Searches for Sentinel 2 datasets
+
+        Parameters:
+            date (str): Date in YYYY-MM-DD format
+            latitude (float): GPS latitude
+            longitude (float): GPS longitude
+            span (int, optional): Maximum span of days to search, defaults to 30.
+            cloudCover (int, optional): Maximum percent of cloud coverage in the tile, defaults to 50.
+            limit (int, optional): Maximum number of returned entries, defaults to 1.
+
+        Returns:
+            list[dict]: list of Sentinel 2 datasets, ordered by newest date
+        """
+        cls.authenticate()
 
         end = datetime.fromisoformat(date) + timedelta(days=1)
         start = end - timedelta(days=span)
 
         query = (
-            f"{__class__.API_URL}"
+            f"{cls.API_URL}"
             f"?startDate={start.isoformat()}Z"
             f"&completionDate={end.isoformat()}Z"
             f"&maxRecords={limit}"
@@ -59,12 +81,31 @@ class SentinelApi:
 
         return data["features"]
 
-    @staticmethod
-    def download_data(id: str, name: str, target_path: str = ""):
+    @classmethod
+    def download_data(cls, id: str, dataset_name: str, target_path: str = None) -> None:
+        """
+        Downloads and extracts a full Sentinel dataset
+
+        Parameters:
+            id (str): ID of the dataset (from Sentinel API)
+            dataset_name (str): Name under which the data should be saved
+            target_path (str, optional): Target path to extract to, defaults to $SENTINEL_DIR.
+        """
+        if target_path is None:
+            target_path = os.environ.get("SENTINEL_DIR", "")
+
+        if (Path(target_path) / dataset_name).exists():
+            print(f"The sentinel dataset '{dataset_name}' has already been downloaded.")
+            return
+
+        os.makedirs(target_path, exist_ok=True)
         zipfile = Path(target_path) / f"{id}.zip"
 
+        cls.authenticate()
+
+        # download zipfile from server and display progress
         with requests.get(
-            __class__.DOWNLOAD_URL.replace("#product_id#", id),
+            cls.DOWNLOAD_URL.replace("#product_id#", id),
             allow_redirects=True,
             stream=True,
             headers={"Authorization": f"Bearer {os.environ["COPERNICUS_TOKEN"]}"},
@@ -73,7 +114,7 @@ class SentinelApi:
             with open(zipfile, "wb") as file:
                 total = int(response.headers.get("content-length"))
                 downloaded = 0
-                print(f"Downloading {round(total / 1048576)} MB to '{target_path}'...")
+                print(f"Downloading {round(total / 1048576)} MB to '{zipfile}'...")
                 for chunk in response.iter_content(chunk_size=16777216):  # 16 MB chunks
                     if chunk:
                         progress = round(downloaded * 100 / total)
@@ -82,15 +123,44 @@ class SentinelApi:
                         downloaded += len(chunk)
                 print("Download complete")
 
-        __class__._extract(zipfile, Path(target_path) / name)
+        cls._extract(zipfile, Path(target_path) / dataset_name)
+
 
     @staticmethod
-    def authenticate():
-        if __class__._is_authenticated():
+    def crop_images(
+        dataset_name: str, latitude: float, longitude: float, parent_path: str = None
+    ) -> None:
+        """
+        Crops all satellite images in the dataset to 100px around the given location to save disk space
+
+        Parameters:
+            dataset_name (str): Name of the dataset folder
+            latitude (float): GPS latitude
+            longitude (float): GPS longitude
+            target_path (str, optional): Parent path of the dataset folder, defaults to $SENTINEL_DIR.
+        """
+        if parent_path is None:
+            parent_path = os.environ.get("SENTINEL_DIR", "")
+
+        path = Path(parent_path) / dataset_name / "IMG_DATA"
+
+        for file in glob.glob("**/*.jp2", root_dir=path, recursive=True):
+            file_path = str(path / file)
+            print(f"Cropping image '{file_path}'...")
+            ImageUtils.crop_location(file_path, file_path, latitude, longitude, 50)
+
+    @classmethod
+    def authenticate(cls) -> None:
+        """
+        Authenticates with the Sentinel API using $COPERNICUS_USER and $COPERNICUS_PASSWORD
+        and saves the authentication token to $COPERNICUS_TOKEN.
+        Authentication is skipped if there is already a valid token.
+        """
+        if cls._is_authenticated():
             return
 
         response = requests.post(
-            __class__.AUTH_URL,
+            cls.AUTH_URL,
             allow_redirects=True,
             data={
                 "username": os.environ["COPERNICUS_USER"],
@@ -110,7 +180,13 @@ class SentinelApi:
         print("Copernicus Dataspace authentication successful")
 
     @staticmethod
-    def _is_authenticated():
+    def _is_authenticated() -> bool:
+        """
+        Checks if the authentication token stored in $COPERNICUS_TOKEN is present and not expired.
+
+        Returns:
+            bool: Returns True if the script is already authenticated
+        """
         if not os.environ.get("COPERNICUS_TOKEN"):
             return False
         if not os.environ.get("COPERNICUS_TOKEN_EXPIRES"):
@@ -118,31 +194,64 @@ class SentinelApi:
         return int(os.environ["COPERNICUS_TOKEN_EXPIRES"]) < time.time()
 
     @staticmethod
-    def _extract(source_path: Path, target_path: Path):
+    def _extract(source_path: Path, target_path: Path) -> None:
+        """
+        Extracts the contents of the sentinel dataset zipfile, deletes unneccesary files
+        and renames the image files for easier access.
+        The following directory structure is achieved:
+        ├── AUX_DATA
+        │   └── <...>
+        ├── IMG_DATA
+        │   ├── R10m
+        │   │   ├── AOT.jp2
+        │   │   ├── B02.jp2
+        │   │   ├── B03.jp2
+        │   │   └── <...>.jp2
+        │   ├── R20m
+        │   │   └── <...>.jp2
+        │   └── R60m
+        │       └── <...>.jp2
+        ├── QI_DATA
+        │   └── <...>
+        ├── MTD_MSIL2A.xml
+        └── MTD_TL.xml
+
+        Parameters:
+            source_path (Path): Path of the downloaded sentinel zipfile
+            target_path (Path): Target folder to extract to
+        """
         print("Extracting...")
         shutil.rmtree(target_path, True)
 
         with ZipFile(source_path, "r") as zip:
-            zip.extractall(target_path)
+            zip.extractall(target_path.parent / source_path.stem)
             os.remove(source_path)
 
-        root = target_path / os.listdir(target_path)[0]
+        # move required files to target directory
+        root = target_path.parent / source_path.stem
+        root = root / os.listdir(root)[0]
         path = root / "GRANULE"
-        path = path / os.listdir(path)[0] / "IMG_DATA"
-        os.rename(path / "R10m", target_path / "R10m")
-        os.rename(path / "R20m", target_path / "R20m")
-        os.rename(path / "R60m", target_path / "R60m")
-        shutil.rmtree(root)
+        path = path / os.listdir(path)[0]
+        os.rename(path, target_path)
+        os.rename(root / "MTD_MSIL2A.xml", target_path / "MTD_MSIL2A.xml")
 
-        for dir in os.listdir(target_path):
-            for file in os.listdir(target_path / dir):
+        # rename image files to "<band>.jp2"
+        img_path = target_path / "IMG_DATA"
+        for dir in os.listdir(img_path):
+            for file in os.listdir(img_path / dir):
                 band = file.split("_")[2]
                 os.rename(
-                    target_path / dir / file,
-                    target_path / dir / f"{band}.jp2".lower(),
+                    img_path / dir / file,
+                    img_path / dir / f"{band}.jp2",
                 )
+        for file in glob.glob("*_PVI.jp2", root_dir=target_path / "QI_DATA"):
+            os.rename(
+                target_path / "QI_DATA" / file,
+                target_path / "QI_DATA" / "PVI.jp2",
+            )
 
-   
+        shutil.rmtree(root.parent)
+
     @staticmethod
     def _check_cloud_pixel(input_file, threshold=20):
         """
@@ -167,3 +276,4 @@ class SentinelApi:
             cloud_present = np.any(region >= threshold)
 
         return cloud_present
+
