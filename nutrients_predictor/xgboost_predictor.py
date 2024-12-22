47718a02ba72da30b892d4b74955ac50624d4770
Viola Kolb <viola.kolb@gmx.de>
2024-12-22 18:21:26 +0100

    Added comments to XGBoost script

--- nutrients_predictor/xgboost_predictor.py
+++ nutrients_predictor/xgboost_predictor.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
+# Path to dataset
 file_path = '/home/ubuntu/AgroLens/Example_Data_combined.csv'
-target = 'K_normalized'    # 'P_normalized', 'K_normalized', 'N_normalized'
-# CSV
+
+# Select target 'P_normalized', 'K_normalized', 'N_normalized'
+target = 'K_normalized'  
+
+# Read CSV
 data = pd.read_csv(file_path)
 
 # Extract the target column
@@ -18,25 +22,15 @@ data = data[feature_columns].values
 
 # Split train and test dataset
 X_train, X_test, y_train, y_test = train_test_split(data, targets, test_size=0.2, random_state=42)
-print('Split train- and testdata')
 
 # Train XGBoost-Modell
-print('Modelltraining')
 model = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.3, learning_rate=0.1,
                          max_depth=5, alpha=10, n_estimators=100)
 model.fit(X_train, y_train)
 
-# Vorhersagen für das Testset
+# Prediction on testset
 y_pred = model.predict(X_test)
 
-# Berechnung des RMSE und R²-Score
+# Calculate MSE
 MSE = mean_squared_error(y_test, y_pred)
-
 print('Mean squared error:',MSE)
-
-# Feature-Importances visualisieren
-# xgb.plot_importance(model, importance_type='weight', max_num_features=10)
-# plt.show()
-
-# Modell speichern (optional)
-#joblib.dump(model, 'xgboost_model.pkl')
